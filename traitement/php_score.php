5f200c28f274374590f5a32a54dba41e1f9f9030
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-07-31 16:33:55 +0200

    calcul du score en fonction du temps

--- traitement/php_score.php
+++ traitement/php_score.php
@@ -3,57 +3,88 @@
     $nb_paire = 3;
     //Données récupérées à la fin du jeu
     $nb_coups = 6;
-    $temps = 6;
+    $temps = 18;
     //Initialisations des variables de comparaison   
         //Variable max points 
         $coups_min = $nb_paire*2;
-        $temps_min = $coups_min;
+        $temps_min = $coups_min;            
+        $max = 500;        
+        //Calcul le nombre de points en fonction du temps 
+        $seconde_temps = 100/$temps_min;                   
+        $score_temps = $max-(($temps-$temps_min)*$seconde_temps);                     
+        if($score_temps>0)   
+            {                         
+                echo $score_temps = round($score_temps);
+            }  
+        else   
+            echo $score_temps = 0;      
+                
 
-        //Variables points dégréssif
+        //Variables temps qui augmente
             //temps            
-            $temps2 = $temps + 2;
+            $temps2 = $temps_min + 2;
             $temps3 = $temps2 + 2;
             //coups
-            $coups2 = $coups_min++;
-            $coups3 = $coups2++;
+            // $coups2 = $coups_min++;
+            // $coups3 = $coups2++;
 
 
     //Implémente coups
-    // for($coups=0; $coups<=$nb_coups; $coups++)
-    //     {
-            //Conditions si nb_coups = coups_min
-             if($nb_coups == $coups_min)
-                {
-                    //Implémente temps_jeu
-                    // for($temps_jeu=0; $temps_jeu<=$temps; $temps_jeu+2)
-                    //     {
-                            //Conditions en fonction du temps 
-                            if($temps == $temps_min)
-                                {
-                                    echo 1000;     
-                                    break;               
-                                }
-                            else if($temps>$temps_min && $temps<=$temps_jeu)
-                                {
-                                    echo 900;  
-                                    break;                 
-                                }
-                        // }
-                    
-                }    
-            //Conditions si nb_coups > coups_min
-            else if($nb_coups == $coups2)
-                {
-                    //Conditions en fonction du temps 
-                    if($temps == $temps_min)
-                        {
-                            echo 900;                   
-                        }
-                    else if($temps>$temps_min && $temps<=$temps2)
-                        {
-                            echo 800;                    
-                        }
-                }
-        // }
+    // for($coups=$coups_min; $coups<=$nb_coups; $coups++)
+    //     {            
+    //         //Conditions si nb_coups = coups_min
+    //          if($nb_coups == $coups && $nb_coups == $coups_min)
+    //             {
+    //                 //Implémente temps_jeu
+    //                 for($temps_jeu=0; $temps_jeu<=$temps; $temps_jeu+2)
+    //                     {
+    //                         //Conditions en fonction du temps 
+    //                         if($temps == $temps_min)
+    //                             {
+    //                                 echo $score = $max;     
+    //                                 break;               
+    //                             }
+    //                         else if($temps>$temps_min && $temps<=$temps2)
+    //                             {
+    //                                 echo $score = $max - $décrémentation;  
+    //                                 break;                 
+    //                             }
+    //                         else if($temps>$temps_min && $temps<=$temps3)
+    //                             {
+    //                                 echo $score = $max - ($décrémentation*2);  
+    //                                 break;                 
+    //                             }
+    //                     }                    
+    //             }    
+    //         //Conditions si nb_coups 
+    //         else if($nb_coups>$coups && $nb_coups<($coups_min+2))
+    //             {
+    //                 //Conditions en fonction du temps 
+    //                 if($temps == $temps_min)
+    //                     {
+    //                         echo $score = 901;      
+    //                         break;             
+    //                     }
+    //                 else if($temps>$temps_min && $temps<=$temps2)
+    //                     {
+    //                         echo $score = 800;  
+    //                         break;                  
+    //                     }
+    //             }
+    //         else if($nb_coups>$coups && $nb_coups<($coups_min+4))
+    //             {
+    //                 //Conditions en fonction du temps 
+    //                 if($temps == $temps_min)
+    //                     {
+    //                         echo $score = 801;      
+    //                         break;             
+    //                     }
+    //                 else if($temps>$temps_min && $temps<=$temps2)
+    //                     {
+    //                         echo $score = 700;  
+    //                         break;                  
+    //                     }
+    //             }
+    //     }
    
 ?>
