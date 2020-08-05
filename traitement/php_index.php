7bb3ad703920d82e4c37302a50b244850be0ad47
celine-pawlak <62133607+celine-pawlak@users.noreply.github.com>
2020-08-05 21:09:11 +0200

    index css

--- traitement/php_index.php
+++ traitement/php_index.php
@@ -2,12 +2,12 @@
 
 if (App::getAuth()->user()) {
     ?>
-    <a class="btn btn-primary jeu" href="memory.php">Nouvelle partie</a>
+    <a class="button" href="memory.php">Nouvelle partie</a>
     <?php
 } else {
     ?>
-    <p class="text-white text-center"><a href="inscription.php" class="index_link">Inscris toi</a> si ce n'est pas déjà fait</p>
-    <p>Sinon <a href="connexion.php" class="index_link">connecte</a>-toi pour jouer</p>
+    <p class="text-white text-center"><a href="inscription.php" class="index_link">Inscris toi</a> si ce n'est pas déjà fait<br>
+    ou <a href="connexion.php" class="index_link">connecte</a>-toi pour jouer</p>
     <?php
 }
 
