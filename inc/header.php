5e222ef0d735a2d809ea97507a39a7b31c341a58
Céline Pawlak <celine.pawlak@laplateforme.io>
2020-08-06 13:53:53 +0200

    responsive

--- inc/header.php
+++ inc/header.php
@@ -13,7 +13,7 @@
              aria-haspopup="true" aria-expanded="false">
             MON COMPTE
         </div>
-        <div class="dropdown-menu"
+        <div class="dropdown-menu dropdown-menu1"
              aria-labelledby="dropdownMenu2">
             <button class="dropdown-item <?= (App::getAuth()->user()->is_admin == 1) ? '' : 'd-none' ?>"
                     type="button"><a class="text-decoration-none" href="admin.php">Espace
@@ -37,49 +37,48 @@
                 class="text-decoration-none"
                 href="inscription.php">INSCRIPTION</a></div>
 </nav>
-
-
 <nav class="navbar_screen navbar_header position-absolute">
     <div class="d-flex flex-column w-100">
-        <div class="dropdown w-100 <?= (App::getAuth()->user(
-        )) ? '' : 'd-none' ?> <?= $page_selected == 'admin' || $page_selected == 'historique' || $page_selected == 'profil' ? 'navbar_selected' : '' ?> flex-grow-1">
-            <div class="dropdown-toggle account w-100" type="button" id="dropdownMenu3" data-toggle="dropdown"
+        <div class="dropdown w-100 flex-grow-1">
+            <div class="dropdown-toggle dropdown-toggle2 w-100 h-100 logoL align-self-left" type="button"
+                 id="dropdownMenu3" data-toggle="dropdown"
                  aria-haspopup="true" aria-expanded="false">
-                <img src="../src/images/LfromLoL.png" class="logoL">
             </div>
-            <div class="dropdown-menu"
+            <div class="dropdown-menu dropdown-menu2"
                  aria-labelledby="dropdownMenu3">
-                <button class="flex-grow-1"><a
-                            class="text-decoration-none <?= $page_selected == 'index' ? 'navbar_selected' : '' ?>"
-                            href="Index.php">ACCUEIL</a></button>
-                <button class="<?= (App::getAuth()->user(
+                <div class="dropdown-item flex-grow-1"><a
+                            class="text-decoration-none"
+                            href="Index.php">ACCUEIL</a></div>
+                <div class="dropdown-item <?= (App::getAuth()->user(
                 )) ? '' : 'd-none' ?> <?= $page_selected == 'memory' ? 'navbar_selected' : '' ?> flex-grow-1"><a
                             class="text-decoration-none" href="memory.php">MEMORY</a>
-                </button>
-                <button class="flex-grow-1"><a
+                </div>
+                <div class="dropdown-item flex-grow-1"><a
                             class="text-decoration-none <?= $page_selected == 'fame' ? 'navbar_selected' : '' ?>"
-                            href="fame.php">WALL OF FAME</a></button>
-
-                <button class="dropdown-item <?= (App::getAuth()->user()->is_admin == 1) ? '' : 'd-none' ?>"
-                        type="button"><a class="text-decoration-none" href="admin.php">Espace
-                        administrateur</a></button>
-                <button class="dropdown-item" type="button"><a class="text-decoration-none" href="historique.php">Voir
-                        historique</a></button>
-                <button class="dropdown-item" type="button"><a class="text-decoration-none" href="profil.php">Modifier
-                        mon compte</a></button>
+                            href="fame.php">WALL OF FAME</a></div>
+                <div class="dropdown-divider <?= (App::getAuth()->user()) ? '' : 'd-none' ?>"></div>
+                <div class="dropdown-item <?= (App::getAuth()->user() and App::getAuth()->user(
+                    )->is_admin == 1) ? '' : 'd-none' ?>"
+                     type="button"><a class="text-decoration-none" href="admin.php">Espace
+                        administrateur</a></div>
+                <div class="dropdown-item <?= (App::getAuth()->user()) ? '' : 'd-none' ?>" type="button"><a class="text-decoration-none" href="historique.php">Voir
+                        historique</a></div>
+                <div class="dropdown-item <?= (App::getAuth()->user()) ? '' : 'd-none' ?>" type="button"><a class="text-decoration-none" href="profil.php">Modifier
+                        mon compte</a></div>
                 <div class="dropdown-divider"></div>
-                <button class="dropdown-item" type="button"><a class="text-decoration-none"
-                                                               href="logout.php">Déconnexion</a>
-                </button>
+                <div class="dropdown-item <?= (App::getAuth()->user()) ? '' : 'd-none' ?>" type="button"><a
+                            class="text-decoration-none"
+                            href="logout.php">Déconnexion</a>
+                </div>
+                <div class="dropdown-item <?= (App::getAuth()->user(
+                )) ? 'd-none' : '' ?> <?= $page_selected == 'connexion' ? 'navbar_selected' : '' ?> flex-grow-1"><a
+                            class="text-decoration-none"
+                            href="connexion.php">Connexion</a></div>
+                <div class="dropdown-item <?= (App::getAuth()->user(
+                )) ? 'd-none' : '' ?> <?= $page_selected == 'inscription' ? 'navbar_selected' : '' ?> flex-grow-1"><a
+                            class="text-decoration-none"
+                            href="inscription.php">Inscription</a></div>
             </div>
         </div>
-        <div class="<?= (App::getAuth()->user(
-        )) ? 'd-none' : '' ?> <?= $page_selected == 'connexion' ? 'navbar_selected' : '' ?> flex-grow-1"><a
-                    class="text-decoration-none"
-                    href="connexion.php">CONNEXION</a></div>
-        <div class="<?= (App::getAuth()->user(
-        )) ? 'd-none' : '' ?> <?= $page_selected == 'inscription' ? 'navbar_selected' : '' ?> flex-grow-1"><a
-                    class="text-decoration-none"
-                    href="inscription.php">INSCRIPTION</a></div>
     </div>
 </nav>
--- index.php
+++ index.php
@@ -7,19 +7,13 @@ require_once 'inc/bootstrap.php';
 <html>
 <head>
     <meta charset="UTF-8">
-          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-            crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-            crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-            integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
-            crossorigin="anonymous"></script>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
           integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+
     <title>Accueil</title>
 </head>
 <body>
@@ -29,7 +23,7 @@ require_once 'inc/bootstrap.php';
 </header>
 <main id="main_index">
     <section id="info_main">
-        <h1 class="title_index">Bienvenue sur le Memory</h1>
+        <h1 class="title_index text-center">Bienvenue sur le Memory</h1>
     </section>
     <section id="reglement" class="d-flex flex-column align-items-center">
         <?php
@@ -98,6 +92,15 @@ require_once 'inc/bootstrap.php';
             </div>
         </div>
     </section>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+            integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+            crossorigin="anonymous"></script>
 </main>
 <footer>
     <?php
